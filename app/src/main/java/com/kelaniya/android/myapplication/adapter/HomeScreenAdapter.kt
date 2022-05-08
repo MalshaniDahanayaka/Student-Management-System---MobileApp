762ef161a4f5ab60c38bcfad2463a3d1cbd878d0
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2022-05-08 12:14:13 +0530

    create adapter to data insert to recycler view in home page

--- app/src/main/java/com/kelaniya/android/myapplication/adapter/HomeScreenAdapter.kt
+++ app/src/main/java/com/kelaniya/android/myapplication/adapter/HomeScreenAdapter.kt
@@ -1,60 +1,60 @@
 package com.kelaniya.android.myapplication.adapter
 
 
-import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
+import androidx.navigation.NavController
 import androidx.recyclerview.widget.RecyclerView
 import com.kelaniya.android.myapplication.R
 import com.kelaniya.android.myapplication.model.Courses
-import com.kelaniya.android.myapplication.model.LecNotes
-import com.kelaniya.android.myapplication.ui.home.HomeFragment
-import java.util.ArrayList
 
 
-class Adapter(val course: Courses): RecyclerView.Adapter<CustomViewHolder>(){
+class Adapter(private val courses: List<Courses>, private val findNavController: NavController): RecyclerView.Adapter<CustomViewHolder>(){
 
 
 
     //number of Items
     override fun getItemCount(): Int {
-        return 18
+        return courses.count()
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val cellForRow = layoutInflater.inflate(R.layout.item_view_design,parent,false)
-        return CustomViewHolder(cellForRow)
+        return CustomViewHolder(cellForRow,findNavController)
     }
 
         override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
 
-            holder.latitude.text  = course.course_id
-            holder.longitude.text = course.course_name
-            holder.name.text = course.lecturer
+            holder.courseID.text  = courses.elementAt(position).course_id
+            holder.subjectName.text = courses.elementAt(position).course_name
+            holder.department.text = courses.elementAt(position).department_name
+            holder.level.text= courses.elementAt(position).level
 
 
     }
 }
 
-class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view){
-    val name: TextView =    itemView.findViewById(R.id.name)
-    val latitude: TextView = itemView.findViewById(R.id.latitude)
-    val longitude: TextView = itemView.findViewById(R.id.longitude)
+class CustomViewHolder(val view: View, private val findNavController: NavController): RecyclerView.ViewHolder(view){
+    val courseID: TextView =    itemView.findViewById(R.id.description_of_lectuer_note)
+    val subjectName: TextView = itemView.findViewById(R.id.subject_name)
+    val department: TextView = itemView.findViewById(R.id.department_name)
+    val level: TextView = itemView.findViewById(R.id.level)
 
 
     init{
         view.setOnClickListener{
-//         val intent = Intent(view.context, MapsActivity::class.java)
-//            intent.putExtra("locationName",name.text)
-//            intent.putExtra("latitude",latitude.text)
-//            intent.putExtra("longitude",longitude.text)
-//
-//
+            findNavController.navigate(R.id.action_nav_home_to_about_course, Bundle().apply {
+                putString("course",courseID.text.toString())
+            })
+
+
+//            val intent = Intent(view.context, HomeFragment::class.java)
+//            intent.putExtra("course_id",courseID.text)
 //            view.context.startActivity(intent)
 
         }
