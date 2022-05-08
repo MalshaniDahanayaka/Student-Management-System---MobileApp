db94c29388d804515e62da9c720dc6e9c7e41d6f
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2022-05-08 12:30:08 +0530

    add home fragment

--- app/src/main/java/com/kelaniya/android/myapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kelaniya/android/myapplication/ui/home/HomeFragment.kt
@@ -2,27 +2,13 @@ package com.kelaniya.android.myapplication.ui.home
 
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.gson.GsonBuilder
-import com.kelaniya.android.myapplication.api.UserAPIService
-import com.kelaniya.android.myapplication.adapter.Adapter
-import com.kelaniya.android.myapplication.api.ApiInterface
-import com.kelaniya.android.myapplication.api.AppInterceptor
 import com.kelaniya.android.myapplication.databinding.FragmentHomeBinding
-import com.kelaniya.android.myapplication.model.Courses
-import com.kelaniya.android.myapplication.model.LecNotes
-import com.kelaniya.android.myapplication.response_data.HomePageData
-import okhttp3.OkHttpClient
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class HomeFragment : Fragment() {
 
@@ -46,12 +32,31 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val homePageData = HomePageData(binding.recyclerView)
-        homePageData.getMyData()
 
-        binding.recyclerView.setBackgroundColor(Color.WHITE)
-        binding.recyclerView.layoutManager = LinearLayoutManager(view.context)
-     //   binding.recyclerView.adapter = Adapter()
+        binding.departmentCourses.setOnClickListener{
+            binding.departmentCourses.setTextColor(Color.GREEN)
+            binding.allCourses.setTextColor(Color.WHITE)
+            val homePageData = HomePageData(binding.recyclerView,findNavController())
+            homePageData.getHomePageData("department")
+
+            //  binding.recyclerView.setBackgroundColor()
+            binding.recyclerView.layoutManager = LinearLayoutManager(view.context)
+            //   binding.recyclerView.adapter = Adapter()
+
+
+        }
+
+        binding.allCourses.setOnClickListener {
+            binding.allCourses.setTextColor(Color.GREEN)
+            binding.departmentCourses.setTextColor(Color.WHITE)
+            val homePageData = HomePageData(binding.recyclerView,findNavController())
+            homePageData.getHomePageData("all")
+
+            //  binding.recyclerView.setBackgroundColor()
+            binding.recyclerView.layoutManager = LinearLayoutManager(view.context)
+            //   binding.recyclerView.adapter = Adapter()
+        }
+
 
 
     }
