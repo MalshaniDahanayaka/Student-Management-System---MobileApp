0a124b13f19f7a417ace4609afeed2ba10622e78
Kasun-se-2018-023 <skasunmk982@gmail.com>
2022-05-08 13:10:56 +0530

    create new fragment for marks and grades

--- app/src/main/java/com/kelaniya/android/myapplication/ui/marks_and_grades/MarksAndGradesFragment.kt
+++ app/src/main/java/com/kelaniya/android/myapplication/ui/marks_and_grades/MarksAndGradesFragment.kt
@@ -1,4 +1,4 @@
-package com.kelaniya.android.myapplication.ui.slideshow
+package com.kelaniya.android.myapplication.ui.marks_and_grades
 
 import android.os.Bundle
 import android.view.LayoutInflater
