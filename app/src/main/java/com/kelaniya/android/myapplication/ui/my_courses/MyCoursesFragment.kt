cd6c36b2bd2321a17eb89ff5325a428664a8ede4
Kasun-se-2018-023 <skasunmk98@gmail.com>
2022-05-16 15:49:53 +0530

    uddate courses fragment

--- app/src/main/java/com/kelaniya/android/myapplication/ui/my_courses/MyCoursesFragment.kt
+++ app/src/main/java/com/kelaniya/android/myapplication/ui/my_courses/MyCoursesFragment.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kelaniya.android.myapplication.databinding.FragmentHomeBinding
 import com.kelaniya.android.myapplication.databinding.FragmentMyCoursesBinding
-
-import com.kelaniya.android.myapplication.utils.GetUserDetails
 
 class MyCoursesFragment : Fragment() {
 
@@ -21,40 +19,29 @@ class MyCoursesFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentMyCoursesBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
 
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.button.setOnClickListener {
-            val email = GetUserDetails().getData(view.context)
-            Toast.makeText(view.context,email.jwtToken, Toast.LENGTH_SHORT).show()
+        binding.enrollCourseRecyclerview.layoutManager = LinearLayoutManager(view.context)
+        EnrollCourseListData(binding.enrollCourseRecyclerview,findNavController()).setEnrollCourseListView()
 
 
-        }
 
 
 
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(MyCoursesViewModel::class.java)
-
-        _binding = FragmentMyCoursesBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
 
 
     override fun onDestroyView() {
