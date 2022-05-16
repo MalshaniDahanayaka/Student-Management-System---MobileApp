523fc7551764ad8cd67cde63b413985e1a777347
Kasun-se-2018-023 <skasunmk98@gmail.com>
2022-05-16 15:54:39 +0530

    update announcement model class

---
+++ app/src/main/java/com/kelaniya/android/myapplication/model/Announcement.kt
@@ -0,0 +1,3 @@
+package com.kelaniya.android.myapplication.model
+
+data class Announcement(var id:Int,var lecturer_email:String,var title:String,var body:String,var category:String,var academic_year:String,var view_state:String)
