26065d7af58010ec2ca121d232ed5ce888fd4bf4
PubuduWickramathunge <pubuduwickramathunge99@gmail.com>
2022-05-16 14:22:26 +0530

    class for get students enroll subjects

--- app/src/main/java/com/kelaniya/android/myapplication/model/StudentsEnrollSubjects.kt
+++ app/src/main/java/com/kelaniya/android/myapplication/model/StudentsEnrollSubjects.kt
@@ -1,3 +1,3 @@
 package com.kelaniya.android.myapplication.model
 
-data class StudentsEnrollSubjects(val student_email:String, val enrolled_course_id:String)
+data class StudentsEnrollSubjects(val student_email:String, val enrolled_course_id:String,val academic_year:String)
