e278fcebbcc22fff2bc69e9907f8f0677b8326d8
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2022-05-18 10:46:53 +0530

    add notification service(show lectures messages

--- app/src/main/java/com/kelaniya/android/myapplication/ui/notifications/NotificationFullDetailsFragment.kt
+++ app/src/main/java/com/kelaniya/android/myapplication/ui/notifications/NotificationFullDetailsFragment.kt
@@ -1,12 +1,12 @@
 package com.kelaniya.android.myapplication.ui.notifications
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.kelaniya.android.myapplication.R
-import com.kelaniya.android.myapplication.databinding.FragmentMyCoursesBinding
 import com.kelaniya.android.myapplication.databinding.FragmentNotificationFullDetailsBinding
 
 
@@ -32,6 +32,12 @@ class NotificationFullDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val notificationDetails = requireArguments().getStringArrayList("announcementDetails") as ArrayList
+        binding.notifiacationDetailsLeturerEmail.text = notificationDetails[2]
+        binding.notificationDetailsTitle.text = notificationDetails[0]+" ( "+ notificationDetails[3]+" )"
+        binding.notificationDetailsBody.text = "                "+ notificationDetails[1]
+        binding.notificationDetailsTitle.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG)
 
     }
 
