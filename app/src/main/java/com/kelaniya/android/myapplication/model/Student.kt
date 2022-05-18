928be2b573d90cd2d5db8d6dc02a991e67f5b8d7
Kasun-se-2018-023 <skasunmk982@gmail.com>
2022-05-18 23:50:36 +0530

    update student entity and navigation according to the profile details update fragment

--- app/src/main/java/com/kelaniya/android/myapplication/model/Student.kt
+++ app/src/main/java/com/kelaniya/android/myapplication/model/Student.kt
@@ -1,5 +1,6 @@
 package com.kelaniya.android.myapplication.model
 
-data class Student (val id:Int, val latitude:String, val longitude:String, val name:String, val age:String, val city:String ,val studentid:String )
+
+data class Student (val student_email:String, val student_id:String, val first_name:String, val last_name:String, val department:String, val profile_pic:String)
 
 
