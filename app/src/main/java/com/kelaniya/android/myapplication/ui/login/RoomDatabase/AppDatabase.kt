8e118a638ffc603f36a48753b898346f8ef35b74
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2022-05-18 10:41:22 +0530

    update signup fragment with signup logics

--- app/src/main/java/com/kelaniya/android/myapplication/ui/login/RoomDatabase/AppDatabase.kt
+++ app/src/main/java/com/kelaniya/android/myapplication/ui/login/RoomDatabase/AppDatabase.kt
@@ -1,9 +1,26 @@
 package com.kelaniya.android.myapplication.ui.login.RoomDatabase
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [UserEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
+
+    companion object{
+        private var INSTANCE:AppDatabase? = null
+
+        fun getAppDatabase(context: Context):AppDatabase?{
+            if(INSTANCE == null){
+                INSTANCE = Room.databaseBuilder<AppDatabase>(
+                    context.applicationContext,AppDatabase::class.java,"usersDatabase"
+                ).allowMainThreadQueries()
+                    .build()
+            }
+
+            return INSTANCE
+        }
+    }
 }
