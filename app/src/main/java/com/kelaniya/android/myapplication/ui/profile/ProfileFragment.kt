a466ffe6776f50e0f9040de0a15b57e5f8cbcd1b
Kasun-se-2018-023 <skasunmk982@gmail.com>
2022-05-18 23:46:14 +0530

    add fragment for update user details

---
+++ app/src/main/java/com/kelaniya/android/myapplication/ui/profile/ProfileFragment.kt
@@ -0,0 +1,70 @@
+package com.kelaniya.android.myapplication.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kelaniya.android.myapplication.databinding.FragmentProfileBinding
+import com.kelaniya.android.myapplication.model.Student
+import com.kelaniya.android.myapplication.ui.login.RoomDatabase.ResponseUserDetails
+import com.kelaniya.android.myapplication.utils.GetUserDetails
+
+
+class ProfileFragment : Fragment() {
+
+
+    private var _binding: FragmentProfileBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val responseUserDetails: ResponseUserDetails = GetUserDetails().getData(view.context)
+        val userRole = responseUserDetails.userRole.toString()
+        if(userRole=="Student"){
+            RetrieveProfileData().getStudentProfileData(binding)
+        }
+        else if(userRole == "Lecturer"){
+
+        }
+
+        binding.userProfileUpdateButton.setOnClickListener {
+            if(userRole=="Student"){
+                val student:Student = Student(
+                                           binding.profileDetailsUserEmail.text.toString(),
+                                           binding.userProfileUserId.text.toString(),
+                                           binding.userProfileFirstName.text.toString(),
+                                           binding.userProfileLastName.text.toString(),
+                                           binding.userProfileDepartment.text.toString(),
+                                  "null"
+                                           )
+                UpdateProfileData().updateStudentsProfileData(student,view.context)
+            }
+            else if(userRole == "Lecturer"){
+
+            }
+
+        }
+
+
+
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/kelaniya/android/myapplication/ui/profile/RetrieveProfileData.kt
@@ -0,0 +1,60 @@
+package com.kelaniya.android.myapplication.ui.profile
+
+import android.os.Bundle
+import android.util.Log
+import com.kelaniya.android.myapplication.R
+import com.kelaniya.android.myapplication.adapter.MarksAndGradesViewAdapter
+import com.kelaniya.android.myapplication.api.RetrofitBuilder
+import com.kelaniya.android.myapplication.databinding.FragmentProfileBinding
+import com.kelaniya.android.myapplication.model.Student
+import com.kelaniya.android.myapplication.model.StudentRecords
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.Exception
+
+class RetrieveProfileData {
+
+    fun getStudentProfileData(binding: FragmentProfileBinding) {
+
+        val retrofitBuilder = RetrofitBuilder().retrofitBuilder
+        var retrofitData = retrofitBuilder.getStudentDetails()
+
+        retrofitData!!.enqueue(object : Callback<Student> {
+            override fun onResponse(call: Call<Student>, response: Response<Student>) {
+
+                val student = response.body()
+
+                try{
+                    binding.profileDetailsUserEmail.setText(student!!.student_email)
+                    binding.userProfileUserId.setText(student.student_id)
+                    binding.userProfileFirstName.setText(student.first_name)
+                    binding.userProfileLastName.setText(student.last_name)
+                    binding.userProfileDepartment.setText(student.department)
+
+                }catch (e: Exception){
+
+                    Log.i("error","empty results")
+                }
+
+            }
+
+            override fun onFailure(call: Call<Student>, t: Throwable) {
+//                findNavController.navigate(R.id.action_nav_home_to_connetion_error)
+//                findNavController.navigate(R.id.action_nav_home_to_connetion_error, Bundle().apply {
+//                    putString("last_location","homePage")
+//                })
+                Log.i("FirstFragment", t.message!!)
+            }
+        })
+
+
+
+    }
+
+    fun getLecturesProfileData(){
+
+
+    }
+
+}
